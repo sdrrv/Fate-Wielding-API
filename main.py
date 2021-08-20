ca5e7ef7d69a24bccccd9ec6c1a6ccf9e4d76bdd
sdrrv <duartecelvas@gmail.com>
2021-08-20 15:25:51 +0100

    Fix

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def index():
 # ex /randomNumberBetween/0/10
 @app.get("/randomNumberBetween")
 def get_random_number_between_two_values(firstValue: int, secondValue: int):
-    return {"response": random.randint(firstValue, secondValue)
+    return {"response": random.randint(firstValue, secondValue),
             "type": "List"}
 
 
@@ -25,5 +25,5 @@ def get_random_choice(amount: Optional[int] = Query(None), value: List[str] = Qu
         amount = 1
     if (amount > len(value)):
         return ["Error, amount greater than the length of the valuesList"]
-    return {"response": random.sample(value, amount)
+    return {"response": random.sample(value, amount),
             "type": "float"}
